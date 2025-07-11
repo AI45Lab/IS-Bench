a0d974bc544e415a0a1abf91d07b582daaa0bb9f
ursulalujun <2441299056@qq.com>
2025-07-11 20:05:59 +0800

    minor revise

--- entrypoints/launcher.sh
+++ entrypoints/launcher.sh
@@ -4,23 +4,23 @@ export OMNIGIBSON_HEADLESS=1
 echo "HEADLESS=1"
 
 #### launcher for slurm ####
-if [[ -v PARTITION ]]; then
-    echo "Submit to $PARTITION"   
-    LAUNCHER+=(
-        "srun" "-p" $PARTITION
-        "--gres=gpu:1"
-        "-N1"
-    )
-fi
+# if [[ -v PARTITION ]]; then
+#     echo "Submit to $PARTITION"   
+#     LAUNCHER+=(
+#         "srun" "-p" $PARTITION
+#         "--gres=gpu:1"
+#         "-N1"
+#     )
+# fi
 
 #### launcher for apptainer or docker ####
-if [[ -v APPTAINER_IMAGE ]]; then
-    LAUNCHER+=("apptainer" "run" "--nv")
-    if [[ -n "${BINDING}" ]]; then
-        LAUNCHER+=("--bind" "${BINDING}")
-    fi
-    LAUNCHER+=("${APPTAINER_IMAGE}")
-fi
+# if [[ -v APPTAINER_IMAGE ]]; then
+#     LAUNCHER+=("apptainer" "run" "--nv")
+#     if [[ -n "${BINDING}" ]]; then
+#         LAUNCHER+=("--bind" "${BINDING}")
+#     fi
+#     LAUNCHER+=("${APPTAINER_IMAGE}")
+# fi
 
 # if [[ -v MAMBA_ROOT_PREFIX ]]; then
 #     LAUNCHER+=("micromamba" "run" "-n" "omnigibson")
