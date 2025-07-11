1b41e6957ac46773563e094a4876f7db37a24324
ursulalujun <2441299056@qq.com>
2025-07-11 20:23:54 +0800

    minor revise

--- entrypoints/env.sh
+++ entrypoints/env.sh
@@ -1,8 +1,14 @@
+# for apptainer
+# export OMNIGIBSON_ROOT=
+# export APPTAINER_IMAGE=
+# export MAMBA_ROOT_PREFIX=/micromamba
+# export APPTAINER_SITE_PACKAGES=/micromamba/envs/omnigibson/lib/python3.10/site-packages
+# export BINDING=
+
 # for slurm
-# export PARTITION=AI4Good_L1 
+export PARTITION=AI4Good_L1_p
 
 # for openai api_key
-
 export OPENAI_API_KEY=your_api_key
 export OPENAI_API_BASE=openai_url
 export GOOGLE_APPLICATION_CREDENTIALS=your_credentials_file
